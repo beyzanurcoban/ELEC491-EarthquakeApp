a61d5558693d4fc21988cc0952e52b8248c95566
KeremG07 <69321438+KeremG07@users.noreply.github.com>
2023-03-29 15:53:40 +0300

    nfc use restricted to uid. changed rw calls to include db.

--- ui/lib/HomePage.dart
+++ ui/lib/HomePage.dart
@@ -30,44 +30,43 @@ class _HomePageState extends State<HomePage> {
             builder: (context, ss) => ss.data != true
                 ? Center(child: Text('NFC is available: ${ss.data}'))
                 : Flex(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              direction: Axis.vertical,
-              children: [
-                Flexible(
-                  flex: 2,
-                  child: Container(
-                    margin: const EdgeInsets.all(4),
-                    constraints: const BoxConstraints.expand(),
-                    decoration: BoxDecoration(border: Border.all()),
-                    child: SingleChildScrollView(
-                      child: ValueListenableBuilder<dynamic>(
-                        valueListenable: result,
-                        builder: (context, value, _) =>
-                            Text('${value ?? ''}'),
-                      ),
-                    ),
-                  ),
-                ),
-                Flexible(
-                  flex: 3,
-                  child: GridView.count(
-                    padding: const EdgeInsets.all(4),
-                    crossAxisCount: 2,
-                    childAspectRatio: 4,
-                    crossAxisSpacing: 4,
-                    mainAxisSpacing: 4,
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    direction: Axis.vertical,
                     children: [
-                      ElevatedButton(
-                          child: Text('NDEF Read'),
-                          onPressed: _tagRead),
-                      ElevatedButton(
-                          child: Text('NDEF Write'),
-                          onPressed: _ndefWrite),
+                      Flexible(
+                        flex: 2,
+                        child: Container(
+                          margin: const EdgeInsets.all(4),
+                          constraints: const BoxConstraints.expand(),
+                          decoration: BoxDecoration(border: Border.all()),
+                          child: SingleChildScrollView(
+                            child: ValueListenableBuilder<dynamic>(
+                              valueListenable: result,
+                              builder: (context, value, _) =>
+                                  Text('${value ?? ''}'),
+                            ),
+                          ),
+                        ),
+                      ),
+                      Flexible(
+                        flex: 3,
+                        child: GridView.count(
+                          padding: const EdgeInsets.all(4),
+                          crossAxisCount: 2,
+                          childAspectRatio: 4,
+                          crossAxisSpacing: 4,
+                          mainAxisSpacing: 4,
+                          children: [
+                            ElevatedButton(
+                                child: Text('Read'), onPressed: _tagRead),
+                            ElevatedButton(
+                                child: Text('Write'),
+                                onPressed: _ndefWrite),
+                          ],
+                        ),
+                      ),
                     ],
                   ),
-                ),
-              ],
-            ),
           ),
         ),
       ),
@@ -76,66 +75,42 @@ class _HomePageState extends State<HomePage> {
 
   void _tagRead() {
     NfcManager.instance.startSession(onDiscovered: (NfcTag tag) async {
-      var ndef = Ndef.from(tag);
-      if (ndef == null) {
-        result.value = 'Tag is not ndef readable';
-        NfcManager.instance.stopSession(errorMessage: result.value);
-        return;
-      }
+      var ndefUID = tag.data["ndef"]["identifier"]
+          .map((e) => e.toRadixString(16).padLeft(2, '0'))
+          .join('');
 
-      try {
-        await ndef.read();
-        if (ndef.cachedMessage == null) {
-          result.value = 'Tag is empty';
-          NfcManager.instance.stopSession(errorMessage: result.value);
-          return;
-        }
-        if (ndef.cachedMessage!.records.isNotEmpty) {
-          NdefRecord textRecord = ndef.cachedMessage!.records.first;
-          result.value = textRecord.toString();
-        } else {
-          result.value = 'Tag is empty';
-        }
-        NfcManager.instance.stopSession();
-      } catch (e) {
-        result.value = e;
-        NfcManager.instance.stopSession(errorMessage: result.value.toString());
-        return;
-      }
+      // TODO: Access database with Unique ID => ndefUID
+
+      result.value = ndefUID;
+
+      NfcManager.instance.stopSession();
     });
   }
 
   void _ndefWrite() {
     NfcManager.instance.startSession(onDiscovered: (NfcTag tag) async {
-      var ndef = Ndef.from(tag);
-      if (ndef == null || !ndef.isWritable) {
-        result.value = 'Tag is not ndef writable';
-        NfcManager.instance.stopSession(errorMessage: result.value);
-        return;
-      }
+      // Get UID
+      var ndefUID = tag.data["ndef"]["identifier"]
+          .map((e) => e.toRadixString(16).padLeft(2, '0'))
+          .join('');
 
       var message = await Navigator.push<String>(
         context,
         MaterialPageRoute(builder: (context) => InputPage()),
       );
 
+      // Return with a canceled message if input is empty
       if (message == null) {
         result.value = 'Canceled';
         NfcManager.instance.stopSession(errorMessage: result.value);
         return;
       }
 
-      var records = [
-        NdefRecord.createText(message),
-      ];
-
-      var ndefMessage = NdefMessage(records);
-
-      // TODO: Buraya database call
-
+      // Write received message to database
       try {
-        await ndef.write(ndefMessage);
-        result.value = 'Success to "Ndef Write"';
+        // TODO: Write message to database with UID (ndefUID)
+
+        result.value = 'Success to "Ndef Write"\nNdefUID: $ndefUID\nMessage: $message';
         NfcManager.instance.stopSession();
       } catch (e) {
         result.value = e;
@@ -144,5 +119,4 @@ class _HomePageState extends State<HomePage> {
       }
     });
   }
-
 }
